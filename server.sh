b5845612339c6cfaca1c594dc842ada6ef4933e4
rqysir609 <rqysir609@gmail.com>
2022-05-03 22:39:20 +0800

    调整结构

--- server.sh
+++ server.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+if[ -f "/home/docker/nginx-compose.yml" ] then
+  echo "Exists"
+else
+  echo "No Exists"
+fi
 #下载nginx默认配置
-curl -O https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx/nginx.conf
+#curl -O https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx/nginx.conf
 #下载nginx默认配置
-curl -o ./conf.d/default.conf --create-dirs https://github.com/rqysir609/docker/blob/main/nginx/nginx/conf.d/default.conf
+#curl -o ./conf.d/default.conf --create-dirs https://github.com/rqysir609/docker/blob/main/nginx/nginx/conf.d/default.conf
 #下载nginx compose文件
-curl -O https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx-compose.yml
+#curl -O https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx-compose.yml
 
 #部署
 
