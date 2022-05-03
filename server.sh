fac7d0bd1ba86bc41b5a6133802eae0f474d948e
rqysir609 <rqysir609@gmail.com>
2022-05-03 13:26:12 +0800

    调整结构

--- server.sh
+++ server.sh
@@ -2,3 +2,7 @@
 
 #初始化swarm集群
 docker swarm init
+
+#下载默认配置
+curl -O https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx/nginx.conf
+curl -o ./conf.d/default.conf --create-dirs https://github.com/rqysir609/docker/blob/main/nginx/nginx/conf.d/default.conf
