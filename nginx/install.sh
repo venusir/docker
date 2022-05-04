32f74a88f95ba74693e8bb72e382bca39d02d283
rqysir609 <rqysir609@users.noreply.github.com>
2022-05-04 16:02:53 +0800

    Update install.sh

--- nginx/install.sh
+++ nginx/install.sh
@@ -2,7 +2,7 @@
 
 #下载nginx默认配置
 if [ ! -f ./nginx.conf ]; then
-  curl -fsSL https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx/nginx.conf -o ./nginx/nginx.conf
+  curl -fsSL https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx/nginx.conf -o ./nginx/nginx.conf --create-dirs
 fi
 
 #下载nginx默认配置
