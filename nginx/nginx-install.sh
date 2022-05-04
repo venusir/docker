7f733b6e7051c6a50aa9ef5815c5b368df968160
rqysir609 <rqysir609@gmail.com>
2022-05-04 15:09:46 +0800

    调整结构

--- nginx/nginx-install.sh
+++ nginx/nginx-install.sh
@@ -2,17 +2,17 @@
 
 #下载nginx默认配置
 if [ ! -f ./nginx.conf ]; then
-  curl -fsSL https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx/nginx.conf -O
+  curl -fsSL https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx/nginx.conf -o ./nginx/nginx.conf
 fi
 
 #下载nginx默认配置
 if [ ! -f ./conf.d/default.conf ]; then
-  curl -fsSL https://raw.githubusercontent.com/rqysir609/docker/main/nginx/conf/conf.d/default.conf -o ./conf.d/default.conf --create-dirs
+  curl -fsSL https://raw.githubusercontent.com/rqysir609/docker/main/nginx/conf/conf.d/default.conf -o ./nginx/conf.d/default.conf --create-dirs
 fi
 
 #下载nginx compose文件
-curl -fsSL https://raw.githubusercontent.com/rqysir609/docker/main/nginx/nginx-install.yml -O
+curl -fsSL https://raw.githubusercontent.com/rqysir609/docker/main/nginx/docker-compose.yml -O
 
 #部署
-docker-compose -f nginx-install.yml up -d
+docker-compose up -d
 
