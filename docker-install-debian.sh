a5b869d8ee531f5a8c129f0f6ca531ccdf444229
rqysir609 <rqysir609@gmail.com>
2022-10-07 17:12:02 +0800

    dddd

--- docker-install-debian.sh
+++ docker-install-debian.sh
@@ -33,7 +33,7 @@ echo \
 sudo apt-get update
 
 #安装最新版本的 Docker Engine-Community 和 containerd
-sudo yes|apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 #设置阿里云镜像加速器
 sudo mkdir -p /etc/docker
