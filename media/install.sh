41b45020381d1e73661505a45007fb416eeb94ff
rqysir609 <rqysir609@gmail.com>
2023-07-06 00:07:53 +0800

    添加nastool

--- media/install.sh
+++ media/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#下载nginx默认配置
+if [ -f ./NasTool.yml ]; then
+  rm -rf NasTool.yml
+fi
+
 #下载nginx compose文件
 curl -fsSL https://raw.githubusercontent.com/rqysir609/docker/main/media/NasTool.yml -O
 
