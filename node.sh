45230d3830bf35f16bc0e7cc9db0b0363f3be1af
rqysir609 <luntan609@hotmail.com>
2023-07-21 13:54:43 +0800

    节点搭建

--- node.sh
+++ node.sh
@@ -5,8 +5,9 @@ apt-get update
 echo "安装Docker"
 # curl -sSL https://get.docker.com/ | sh
 
-mkdir /etc/nginx
-mkdir /etc/nginx/conf.d
+mkdir -p /etc/nginx
+mkdir -p /etc/nginx/conf.d
+mkdir -p /usr/share/nginx/html
 
 CF_Token="AAA"
 CF_Zone_ID="AAA"
@@ -15,9 +16,9 @@ CF_Account_ID="AAA"
 # 生成容器
 docker run --name nginx -p 9001:80 -d nginx
 # 将容器nginx.conf文件复制到宿主机
-docker cp nginx:/etc/nginx/nginx.conf /etc/nginx/conf/nginx.conf
+docker cp nginx:/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 # 将容器conf.d文件夹下内容复制到宿主机
-docker cp nginx:/etc/nginx/conf.d /etc/nginx/conf/conf.d
+docker cp nginx:/etc/nginx/conf.d /etc/nginx/conf.d
 # 将容器中的html文件夹复制到宿主机
 docker cp nginx:/usr/share/nginx/html /usr/share/nginx/html
 # 删除正在运行的nginx容器
