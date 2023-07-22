d8c7748c7bf266a9afe1b6c60a9cb4bf51c3d40e
rqysir609 <luntan609@hotmail.com>
2023-07-22 12:25:49 +0800

    节点搭建

--- node.sh
+++ node.sh
@@ -50,7 +50,7 @@ server {
 		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 		proxy_set_header Host \$http_host;
 		proxy_redirect off;
-		proxy_pass http://127.0.0.1:8080;
+		proxy_pass http://127.0.0.1:54321;
 	}
 
 	# location /ray {   #分流路径
