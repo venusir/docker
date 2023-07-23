afe7443c3f2d04f7f966101a52f467fa3761b6f3
rqysir609 <rqysir609@gmail.com>
2023-07-23 22:51:53 +0800

    节点搭建

--- node.sh
+++ node.sh
@@ -24,7 +24,7 @@ apt-get install curl
 # 启用 BBR TCP 拥塞控制算法
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p-
+sysctl -p
 
 # 安装x-ui面板
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
