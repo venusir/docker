d5047f5fc6449705cd99e8ff54aff3bcbca15ce9
rqysir609 <rqysir609@gmail.com>
2024-03-14 22:02:07 +0800

    dns

--- dns_server/dns.sh
+++ dns_server/dns.sh
@@ -3,7 +3,7 @@
 # 下载 mosdns
 wget https://github.com/IrineSistiana/mosdns/releases/download/v5.1.3/mosdns-linux-amd64.zip
 # 下载配置
-wget https://github.com/rqysir609/docker/raw/main/dns/dns.zip
+wget https://github.com/rqysir609/docker/raw/main/dns_server/dns.zip
 
 # 解压配置
 unzip dns.zip
