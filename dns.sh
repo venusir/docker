2ed969159f5b7d996600c91db2704523555f6d57
rqysir609 <rqysir609@gmail.com>
2024-03-14 20:56:17 +0800

    dns+v2raya

---
+++ dns.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 下载 mosdns
+wget https://github.com/IrineSistiana/mosdns/releases/download/v5.1.3/mosdns-linux-amd64.zip
+# 下载配置
+wget https://github.com/rqysir609/docker/raw/main/dns+v2raya/dns+v2raya.zip
